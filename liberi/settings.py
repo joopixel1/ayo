5a1e110ceeb742a9e1256010bdc66db5c85ccb03
joopixel1 <joo@iastate.edu>
2022-08-16 10:50:46 -0500

    white

--- liberi/settings.py
+++ liberi/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'phone_field',
     'crispy_forms',
+    'whitenoise.runserver_nostatic', # new!
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # new!
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,5 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
